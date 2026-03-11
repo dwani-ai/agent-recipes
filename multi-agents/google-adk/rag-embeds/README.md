3d66fc68f3b0e76929631a7ef8788b4ece29b3a7
sachin shetty <26170834+sachinsshetty@users.noreply.github.com>
2026-03-11 07:24:30 +0100

    embedded-gemma0examples

--- multi-agents/google-adk/rag-embeds/README.md
+++ multi-agents/google-adk/rag-embeds/README.md
@@ -1,5 +1,19 @@
 RAG + Embeds
 
-Embedding Gemma - https://ai.google.dev/gemma/docs/embeddinggemma/inference-embeddinggemma-with-sentence-transformers
+- EmbeddedGemma
+    - https://ai.google.dev/gemma/docs/embeddinggemma 
 
+    - https://ai.google.dev/gemma/docs/embeddinggemma/inference-embeddinggemma-with-sentence-transformers
+
+    - Prompt Instructions - 
+        - https://ai.google.dev/gemma/docs/embeddinggemma/model_card#prompt_instructions
+
+- Install and Run 
+
+```bash
 pip install -U sentence-transformers git+https://github.com/huggingface/transformers@v4.56.0-Embedding-Gemma-preview
+```
+
+```
+python embedded-gemma-example.py
+```
--- multi-agents/google-adk/rag-embeds/embedded-gemma-example.py
+++ multi-agents/google-adk/rag-embeds/embedded-gemma-example.py
@@ -11,7 +11,7 @@ print(f"Device: {model.device}")
 print(model)
 print("Total number of parameters in the model:", sum([p.numel() for _, p in model.named_parameters()]))
 
-"""
+
 ## Step 2
 
 words = ["apple", "banana", "car"]
@@ -22,12 +22,10 @@ embeddings = model.encode(words)
 print(embeddings)
 for idx, embedding in enumerate(embeddings):
   print(f"Embedding {idx+1} (shape): {embedding.shape}")
-"""
 
 
-"""
 
-Step 3
+##Step 3
 
 # The sentences to encode
 sentence_high = [
@@ -50,4 +48,79 @@ for sentence in [sentence_high, sentence_medium, sentence_low]:
   similarities = model.similarity(embeddings[0], embeddings[1])
   print("`-> 🤖 score: ", similarities.numpy()[0][0])
 
-"""
+
+## Step 4 - Using Prompts for Tasks
+
+print("Available tasks:")
+for name, prefix in model.prompts.items():
+  print(f" {name}: \"{prefix}\"")
+print("-"*80)
+
+for sentence in [sentence_high, sentence_medium, sentence_low]:
+  print("🙋‍♂️")
+  print(sentence)
+  embeddings = model.encode(sentence, prompt_name="STS")
+  similarities = model.similarity(embeddings[0], embeddings[1])
+  print("`-> 🤖 score: ", similarities.numpy()[0][0])
+
+
+### Step 5 - Classification
+
+labels = ["Billing Issue", "Technical Support", "Sales Inquiry"]
+
+sentence = [
+  "Excuse me, the app freezes on the login screen. It won't work even when I try to reset my password.",
+  "I would like to inquire about your enterprise plan pricing and features for a team of 50 people.",
+]
+
+# Calculate embeddings by calling model.encode()
+label_embeddings = model.encode(labels, prompt_name="Classification")
+embeddings = model.encode(sentence, prompt_name="Classification")
+
+# Calculate the embedding similarities
+similarities = model.similarity(embeddings, label_embeddings)
+print(similarities)
+
+idx = similarities.argmax(1)
+print(idx)
+
+for example in sentence:
+  print("🙋‍♂️", example, "-> 🤖", labels[idx[sentence.index(example)]])
+
+
+## Step -5 - Matryoshka Representation Learning (MRL)
+def check_word_similarities():
+  # Calculate the embedding similarities
+  print("similarity function: ", model.similarity_fn_name)
+  similarities = model.similarity(embeddings[0], embeddings[1:])
+  print(similarities)
+
+  for idx, word in enumerate(words[1:]):
+    print("🙋‍♂️ apple vs.", word, "-> 🤖 score: ", similarities.numpy()[0][idx])
+
+# Calculate embeddings by calling model.encode()
+embeddings = model.encode(words, prompt_name="STS")
+
+check_word_similarities()
+
+###
+
+embeddings = model.encode(words, truncate_dim=512, normalize_embeddings=True)
+
+for idx, embedding in enumerate(embeddings):
+  print(f"Embedding {idx+1}: {embedding.shape}")
+
+print("-"*80)
+check_word_similarities()
+
+
+###
+
+model = SentenceTransformer(model_id, truncate_dim=256, similarity_fn_name="dot").to(device=device)
+embeddings = model.encode(words, prompt_name="STS", normalize_embeddings=True)
+
+for idx, embedding in enumerate(embeddings):
+  print(f"Embedding {idx+1}: {embedding.shape}")
+
+print("-"*80)
+check_word_similarities()
